50c8982867ec118b962bb57e19700f97522fb78b
hasnainworktime <hasnain.workday@gmail.com>
2026-01-29 15:47:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function showSurprise() {
     document.getElementById("surprise").innerText =
         "🎉 آپ جیسے لوگ زندگی کو خوبصورت بنا دیتے ہیں 😊";
 }
+function playVoice() {
+    document.getElementById("birthdayAudio").play();
+}
